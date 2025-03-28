6d72709906d98131429e417a348ddd16fa01b5f9
Filipe <163340243+filipe-macedo@users.noreply.github.com>
2025-03-28 15:59:46 -0300

    Aula 2
    Codigo implementado durante a aula

--- main.py
+++ main.py
@@ -1,16 +1,6 @@
-import requests
 import pandas as pd
+from src.extractTransform import requestApiBcb
+from src.load import salvarCsv
 
-
-def requestsApiBcb(data):
-    """Funcao para extrair os dados dos meios de pagamentos trimestrais do Banco Central
-    Parametros:
-    Data - string - aaaat (Ex:20191)"""
-    
-    url = f"https://olinda.bcb.gov.br/olinda/servico/MPV_DadosAbertos/versao/v1/odata/MeiosdePagamentosTrimestralDA(trimestre=@trimestre)?@trimestre='{data}'&$format=json"
-    req = requests.get(url)
-    dados = req.json()
-    df = pd.json_normalize(dados['value'])
-    return print(df)
-
-requestsApiBcb(20241)
+dadosBcb = requestApiBcb('20191')
+salvarCsv(dadosBcb, "src/datasets/meiosPagamentosTri.csv", ";", ".")

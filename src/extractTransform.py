e0b828eedafa1f1d79e512af011f0b16d6de3624
Filipe <163340243+filipe-macedo@users.noreply.github.com>
2025-04-04 15:55:15 -0300

    Conteudo da aula 3

--- src/extractTransform.py
+++ src/extractTransform.py
@@ -3,15 +3,15 @@ import pandas as pd
 
 
 def requestApiBcb(data: str) -> pd.DataFrame:
-    """Funcao para extrair os dados dos meios de pagamentos trimestrais do Banco Central 
+    """Funcao para extrair os dados dos meios de pagamentos trimestrais do Banco Central
     Parametros: Data - string - aaaat (Ex:20191)
     Saida: DataFrame - Estrutura de dados do Pandas"""
-    
-    url = f"https://olinda.bcb.gov.br/olinda/servico/MPV_DadosAbertos/versao/v1/odata/MeiosdePagamentosTrimestralDA(trimestre=@trimestre)?@trimestre=%27{data}%27&$format=json" 
+
+    url = f"https://olinda.bcb.gov.br/olinda/servico/MPV_DadosAbertos/versao/v1/odata/MeiosdePagamentosTrimestralDA(trimestre=@trimestre)?@trimestre=%27{data}%27&$format=json"
     req = requests.get(url)
     print("Status Code:", req.status_code)
     dados = req.json()
 
     df = pd.json_normalize(dados["value"])
-    df['datatrimestre'] = pd.to_datetime(df['datatrimestre'])
+    df["datatrimestre"] = pd.to_datetime(df["datatrimestre"])
     return df
--- src/load.py
+++ src/load.py
@@ -1,4 +1,7 @@
 import pandas as pd
+import sqlite3
+from sqlalchemy import create_engine
+
 
 def salvarCsv(df: pd.DataFrame, nome_arquivo: str, separador: str, decimal: str):
     """
@@ -11,4 +14,24 @@ def salvarCsv(df: pd.DataFrame, nome_arquivo: str, separador: str, decimal: str)
     decimal (str, opcional): Caractere usado para representar casas decimais. Padrão é ".".
     """
     df.to_csv(nome_arquivo, sep=separador, decimal=decimal)
+    return
+
+
+def salvarSQLite(df: pd.DataFrame, nome_banco: str, nome_tabela: str):
+    conn = sqlite3.connect(nome_banco)
+    df.to_sql(nome_tabela, conn, if_exists="replace", index=False)
+    conn.close()
+    return
+
+
+def salvarMySQL(
+    df: pd.DataFrame,
+    usuario: str,
+    senha: str,
+    host: str,
+    nome_banco: str,
+    nome_tabela: str,
+):
+    engine = create_engine(f"mysql+pymysql://{usuario}:{senha}@{host}/{nome_banco}")
+    df.to_sql(nome_tabela, con=engine, if_exists="replace", index=False)
     return

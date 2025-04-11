dde944eb47b10586b7999a8294774d0ac8a39a26
Filipe <163340243+filipe-macedo@users.noreply.github.com>
2025-04-11 16:28:10 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -5,6 +5,6 @@ from src.load import salvarCsv, salvarSQLite, salvarMySQL
 dadosBcb = requestApiBcb("20191")
 # salvarCsv(dadosBcb, "src/datasets/meiosPagamentosTri.csv", ";", ".")
 
-# salvarSQLite(dadosBcb, "src/datasets/etlbcb.db", "meios_pagamentos_tri")
+salvarSQLite(dadosBcb, "src/datasets/etlbcb.db", "meios_pagamentos_tri")
 
-salvarMySQL(dadosBcb, "root", "teste", "localhost", "etlbcb", "meios_pagamentos_tri")
+# salvarMySQL(dadosBcb, "root", "teste", "localhost", "etlbcb", "meios_pagamentos_tri")
